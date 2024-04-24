47cff517a7fad923e7e2aa589a231f001abf1874
Lawrence Jones <lawrencejones1234@icloud.com>
2024-04-23 22:34:59 -0500

    Problem 1 Redo

--- Problem1.rb
+++ Problem1.rb
@@ -1,11 +1,16 @@
 require 'httparty'
 require 'json'
 
-response = HTTParty.get("https://api.weatherapi.com/v1/current.json?key=5c8c38aab4a7438187a50458242803&q=Memphis&days=1&aqi=no&alerts=no")
+response = HTTParty.get("https://api.weatherapi.com/v1/forecast.json?key=5c8c38aab4a7438187a50458242803&q=Memphis&days=1&aqi=no&alerts=no")
 
 if response
+    puts "Location: #{response['location']['name']}, #{response['location']['region']}"
     puts "Current temperature: #{response['current']['temp_f']}"
     puts "Humidity: #{response['current']['humidity']}"
     puts "Weather conditions: #{response['current']['condition']['text']}"
-    puts "Average temperature: "
+
+    hourly_forecast = response['forecast']['forecastday'][0]['hour']
+    total_temperature = hourly_forecast.map { |hour| hour['temp_f'] }.sum
+    average_temperature = total_temperature / hourly_forecast.size.to_f
+    puts "Average temperature (hourly): #{average_temperature.round(1)}"
 end
