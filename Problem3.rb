291320b77c684a03ec8e27ee1184a8796fb7511a
Lawrence Jones <lawrencejones1234@icloud.com>
2024-04-23 22:35:32 -0500

    Problem 3 redo

--- Problem3.rb
+++ Problem3.rb
@@ -1,4 +1,23 @@
 require 'httparty'
 require 'json'
 
-response = HTTParty.get("https://app.ticketmaster.com/discovery/v2")
+response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=wHFVqGkPGVFrddqkDikdHmlPWgpzF0Za&city=Memphis")
+
+if response
+    puts "Events in Memphis, TN"
+    puts ""
+    
+    events = response["_embedded"]["events"]
+    events.each do |event|
+        name = event["name"]
+        venue = event["_embedded"]["venues"][0]["name"]
+        date = event["dates"]["start"]["localDate"]
+        time = event["dates"]["start"]["localTime"]
+
+        puts "Event: #{name}"
+        puts "Venue: #{venue}"
+        puts "Date: #{date}"
+        puts "Time: #{time}"
+        puts ""
+    end
+end
